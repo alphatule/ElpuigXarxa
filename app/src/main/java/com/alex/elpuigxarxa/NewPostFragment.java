4134b82b1a98e0e008eac39b59ae02cc5ea6fdae
Alphatule <66392548+alphatule@users.noreply.github.com>
2025-03-11 17:54:55 +0100

    wtf

--- app/src/main/java/com/alex/elpuigxarxa/NewPostFragment.java
+++ app/src/main/java/com/alex/elpuigxarxa/NewPostFragment.java
@@ -2,11 +2,33 @@ package com.alex.elpuigxarxa;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.appwrite.Client;
+import io.appwrite.coroutines.CoroutineCallback;
+import io.appwrite.exceptions.AppwriteException;
+import io.appwrite.models.User;
+import io.appwrite.services.Account;
+import io.appwrite.services.Databases;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +37,90 @@ import android.view.ViewGroup;
  */
 public class NewPostFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button publishButton;
+    EditText postContentEditText;
+    NavController navController;
+    Client client;
+    Account account;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NewPostFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewPostFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewPostFragment newInstance(String param1, String param2) {
-        NewPostFragment fragment = new NewPostFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_post, container, false);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        client = new Client(requireContext())
+                .setProject(getString(R.string.APPWRITE_PROJECT_ID));
+        publishButton = view.findViewById(R.id.publishButton);
+        postContentEditText = view.findViewById(R.id.postContentEditText);
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publicar();
+            }
+        });
+    }
+
+    private void publicar(){
+        String postContent = postContentEditText.getText().toString();
+        if(TextUtils.isEmpty(postContent)){
+            postContentEditText.setError("Required");
+            return;
+        }
+        publishButton.setEnabled(false); // Obtenemos información de la cuenta del autor
+        account = new Account(client);
+        try {
+            account.get(new CoroutineCallback<>((result, error) -> {
+                if (error != null) {
+                    error.printStackTrace();
+                    return;
+                }
+                guardarEnAppWrite(result, postContent);
+            }));
+        } catch (AppwriteException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_post, container, false);
+    void guardarEnAppWrite(User<Map<String, Object>> user, String content) {
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        // Crear instancia del servicio Databases
+        Databases databases = new Databases(client);
+        // Datos del documento
+        Map<String, Object> data = new HashMap<>();
+        data.put("uid", user.getId().toString());
+        data.put("author", user.getName().toString());
+        data.put("authorPhotoUrl", null);
+        data.put("content", content);
+        // Crear el documento
+        try {
+            databases.createDocument(
+                    getString(R.string.APPWRITE_DATABASE_ID),
+                    getString(R.string.APPWRITE_POSTS_COLLECTION_ID),
+                    "unique()", // Generar un ID único automáticamente
+                    data,
+                    new ArrayList<>(), // Permisos opcionales, como ["role:all"]
+                    new CoroutineCallback<>((result, error) -> {
+                        if (error != null) {
+                            Snackbar.make(requireView(), "Error: " +
+                                    error.toString(), Snackbar.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            System.out.println("Post creado:" + result.toString());
+                            mainHandler.post(() ->
+                            {
+                                navController.popBackStack();
+                            });
+                        }
+                    })
+            );
+        } catch (AppwriteException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

2bab3fba0126a9aaa3b0119bbf649d5b37cf4d2b
Alphatule <66392548+alphatule@users.noreply.github.com>
2025-03-11 16:01:25 +0100

    mierdon

---
+++ app/src/main/java/com/example/p10/HomeFragment.java
@@ -0,0 +1,85 @@
+package com.example.p10;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
+
+import io.appwrite.Client;
+import io.appwrite.coroutines.CoroutineCallback;
+import io.appwrite.exceptions.AppwriteException;
+import io.appwrite.services.Account;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+    private NavController navController;
+
+    public HomeFragment() {
+    }
+
+    ImageView photoImageView;
+    TextView displayNameTextView, emailTextView;
+    Client client;
+    Account account;
+    String userId;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        NavigationView navigationView = view.getRootView().findViewById(R.id.nav_view);
+        View header = navigationView.getHeaderView(0);
+        photoImageView = header.findViewById(R.id.imageView);
+        displayNameTextView = header.findViewById(R.id.displayNameTextView);
+        emailTextView = header.findViewById(R.id.emailTextView);
+        client = new Client(requireContext()).setProject(getString(R.string.APPWRITE_PROJECT_ID));
+        account = new Account(client);
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        try {
+            account.get(new CoroutineCallback<>((result, error) -> {
+                if (error != null) {
+                    error.printStackTrace();
+                    return;
+                }
+                mainHandler.post(() ->
+                {
+                    userId = result.getId();
+                    displayNameTextView.setText(result.getName().toString());
+                    emailTextView.setText(result.getEmail().toString());
+                    Glide.with(requireView()).load(R.drawable.user).into(photoImageView);
+                });
+            }));
+        } catch (AppwriteException e) {
+            throw new RuntimeException(e);
+        }
+
+        view.findViewById(R.id.gotoNewPostFragmentButton).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 navController.navigate(R.id.newPostFragment);
+             }
+        });
+    }
+}
